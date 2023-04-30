de74ed62252b35b518c61ee05e10591237723445
baozi <bigbaozi@126.com>
2023-04-30 20:07:41 +0800

    Ip2region使用学习

---
+++ src/main/java/com/baozi/ip2region/Ip2regionStudy.java
@@ -0,0 +1,51 @@
+package com.baozi.ip2region;
+
+import org.lionsoul.ip2region.xdb.Searcher;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 创建人：baozi
+ * 创建日期：2023/4/30 10:09
+ * 描述：https://github.com/lionsoul2014/ip2region/tree/master/binding/java
+ */
+public class Ip2regionStudy {
+    public static void main(String[] args) throws IOException {
+        String dbPath = "/Users/baoqingyang/study/utils-study/src/main/resources/ip2regionFiles/ip2region.xdb";
+
+        // 1、从 dbPath 加载整个 xdb 到内存。
+        byte[] cBuff;
+        try {
+            cBuff = Searcher.loadContentFromFile(dbPath);
+        } catch (Exception e) {
+            System.out.printf("failed to load content from `%s`: %s\n", dbPath, e);
+            return;
+        }
+
+        // 2、使用上述的 cBuff 创建一个完全基于内存的查询对象。
+        Searcher searcher;
+        try {
+            searcher = Searcher.newWithBuffer(cBuff);
+        } catch (Exception e) {
+            System.out.printf("failed to create content cached searcher: %s\n", e);
+            return;
+        }
+
+        // 3、查询
+        String ip = "119.113.140.18";
+        try {
+            long sTime = System.nanoTime();
+            String region = searcher.search(ip);
+            long cost = TimeUnit.NANOSECONDS.toMicros((long) (System.nanoTime() - sTime));
+            System.out.printf("{region: %s, ioCount: %d, took: %d μs}\n", region, searcher.getIOCount(), cost);
+        } catch (Exception e) {
+            System.out.printf("failed to search(%s): %s\n", ip, e);
+        }
+
+        // 4、关闭资源 - 该 searcher 对象可以安全用于并发，等整个服务关闭的时候再关闭 searcher
+         searcher.close();
+
+        // 备注：并发使用，用整个 xdb 数据缓存创建的查询对象可以安全的用于并发，也就是你可以把这个 searcher 对象做成全局对象去跨线程访问。
+    }
+}
